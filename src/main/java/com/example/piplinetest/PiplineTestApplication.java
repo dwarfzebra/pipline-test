bd35bccd35e33d451a2a19c1554e1d4bb7e17481
xueyongshuai <xueyongshuai@taoche.com>
2023-08-10 15:17:22 +0800

    Initial commit

---
+++ src/main/java/com/example/piplinetest/PiplineTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.piplinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplineTestApplication.class, args);
+    }
+
+}
